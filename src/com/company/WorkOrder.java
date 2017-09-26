9b43f944312a4d60048d4c7a1874fc04252f39d2
Justin Kim <jjkim928@gmail.com>
2017-09-25 20:58:39 -0400

    basic Javabean

---
+++ src/com/company/WorkOrder.java
@@ -0,0 +1,40 @@
+package com.company;
+
+public class WorkOrder {
+    public int id;
+    public String description;
+    public String senderName;
+    public Status status;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSenderName() {
+        return senderName;
+    }
+
+    public void setSenderName(String senderName) {
+        this.senderName = senderName;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+}
