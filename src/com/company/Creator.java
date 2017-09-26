082db7e602453e84f59a13ba14f320a045194a99
Justin Kim <jjkim928@gmail.com>
2017-09-26 18:51:19 -0400

    created function to set the workOrder

--- src/com/company/Creator.java
+++ src/com/company/Creator.java
@@ -1,11 +1,32 @@
 package com.company;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Creator {
-    public void createWorkOrders() {
+    public void createWorkOrders() throws IOException {
         // read input, create work orders and write as json files
+        Scanner input = new Scanner(System.in);
+        WorkOrder workOrder = new WorkOrder();
+        int workID = 1;
+
+        System.out.println("Name: ");
+        workOrder.setSenderName(input.nextLine());
+        System.out.println("Description: ");
+        workOrder.setDescription(input.nextLine());
+        workOrder.setStatus(Status.INITIAL);
+        workOrder.setId(workID);
+
+        workID++;
+
+        ObjectMapper map = new ObjectMapper();
+        map.writeValue(new File(workID + ".json"), workOrder);
     }
 
-    public static void main(String args[]) {
+    public static void main(String args[]) throws IOException {
         Creator creator = new Creator();
         creator.createWorkOrders();
     }
